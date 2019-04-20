65c3ab1512c1e68c75d266629ff615fd2d5c71e4
Slava Kurganov <sgorelchik@mail.ru>
2019-04-20 10:25:21 +0300

    Начата работа на классом-работником

--- AntivirusLibrary/AntivirusWorker.cs
+++ AntivirusLibrary/AntivirusWorker.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AntivirusLibrary.Abstracts;
+using AntivirusLibrary.Files;
 
 namespace AntivirusLibrary
 {
     public class AntivirusWorker
     {
+        internal List<FileWithSignature> DangerFiles;
     }
 }
