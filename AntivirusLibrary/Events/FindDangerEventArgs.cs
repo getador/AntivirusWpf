2d24c0dc622027b2174ec92e53ce8d125437203b
Slava Kurganov <sgorelchik@mail.ru>
2019-04-22 14:27:52 +0300

    Создано событие на нахождение вируса

---
+++ AntivirusLibrary/Events/FindDangerEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntivirusLibrary.Abstracts;
+
+namespace AntivirusLibrary.Events
+{
+    class FindDangerEventArgs:EventArgs
+    {
+        public FindDangerEventArgs(FileWithSignature dangerFile)
+        {
+            DangerFile = dangerFile;
+        }
+
+        public FileWithSignature DangerFile { get; }
+    }
+}
