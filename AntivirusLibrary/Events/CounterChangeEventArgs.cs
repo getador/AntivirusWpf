73596c29cf3fffd6dd05f3025dfeedbbf54bbc44
Slava Kurganov <sgorelchik@mail.ru>
2019-04-29 12:47:59 +0300

    Добавлено событие

---
+++ AntivirusLibrary/Events/CounterChangeEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntivirusLibrary.Events
+{
+    public class CounterChangeEventArgs:EventArgs
+    {
+        public double Count { get; set; }
+        public CounterChangeEventArgs(double count)
+        {
+            Count = count;
+        }
+    }
+}
